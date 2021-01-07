129e0f94d2e6a1c0f2db8625a3c0799f0e713e88
Diógenes Dias <diogenesdias@hotmail.com>
2021-01-07 07:55:30 -0300

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,8 @@
 // \__ \ | | (_-< |  _| / -_) | '  \  / _` |
 // |___/ |_| /__/  \__| \___| |_|_|_| \__,_|
 //                                          
+// Define o conjunto de caracteres a ser utilizado 
+// Alterne entre UTF-8 e ISO-8859-1
 header("Content-Type: text/html; charset=iso-8859-1", true);
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'portuguese');
@@ -14,30 +16,52 @@ ini_set('default_charset', 'ISO-8859-1');
 // /_/ \_\ | .__/ |_| |_| \__| \__,_| \__| \__,_| \___/
 //         |_|                         )_)             
 //
+//
+// Define o caminho raiz da aplicação
 define('ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+// Nome da aplicação que é exibido nas seguintes janelas:
+// 1 - Título da janela de login
+// 2 - Título da janela de Esqueci a Senha
+// 3 - No header da aplicação no topo da página.
+// 4 - No header da janela Sobre...
+// 
 define("APP_NAME", "SCW");
+// Define a versão da aplição que é exibida no rodapé
 define("VERSION", "2.0.3 Beta");
+// Define o título da janela das páginas da aplicação
 define("TITLE", APP_NAME . " - Sistema Comercial Web - versão. " . VERSION);
+// Define a data da última atualização realizada no config da aplicação
 define("LAST_DATE", " " . date("d M Y H:i:s", filemtime(ROOT . "config.php")));
+// Definei a mensagem de Copyright que será exibida no rodapé da aplicação
 define("COPY", date('Y') . " &copy; <a href=\"http://www.ipage.com.br\" target=\"_blank\">Ipage Software</a> - Todos os direitos reservados");
-// Não modificar
+// Define o link para a cahamda ao webservice que irá validar a licença de uso desta aplicação
+// Nota: NÃO MODIFICAR
 define('URL_WEBSERVICE_SETTINGS', "https://www.ipage.com.br/framework/lavorox/accesskey/");
-// Não modificar
+// Chave utilizada pelo método encrypt no arquivo function
 define('COOKIE_KEY', 'TSZsyrf6FfqbyHCIpfnpqD771XxwBbGpujt07LFLhVeMNSQyzMCUsONC'); 
-// Chave responsável pela licença de uso
+// Chave responsável pela licença de uso da aplicação
+// para solicitar sua licença de uso, favor realizar um pré-cadastro no seguinte endereço:
+// https://www.ipagesoftware.com.br/license_key/www/lic_pre_cadastro/
 define("ACCESS_KEY", "COLOQUE AQUI O SUA CHAVE");
 // Solicite a sua chave desta api no link abaixo:
 // https://ipage.com.br/ipage/wskey_produto/
 define("API_PRODUTOS", "1f1f510b7cf211ea8dca525400c9158a");
-
-define('INITIAL_PAGINATION_NUMBER', 5);
-define('NO_RIGHT_MOUSE_BUTTON', 0); //true/1 bloqueia o botão direito do mouse
+// Define o número da página inicial ao listar os registro na tabela
+define('INITIAL_PAGINATION_NUMBER', 9);
+// Define a seleção da paginação
+define('PAGINATION', [INITIAL_PAGINATION_NUMBER, 3, 6, 9, 12, 25, 50, 100, 250, 'Todos os Reg.']);
+ // 0 - Permite o menu de contexto ao se pressionar o botão direito do mouse
+ // 1 - Bloqueio o menu de contexto ao se pressionar o botão direito do mouse
+define('NO_RIGHT_MOUSE_BUTTON', 0);
+// Retorna a url atual
 define('URL_ATUAL', $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING']);
-// 1 Entra direto na página cadastro, 0 entra na página que exibe as planilhas
+// 0 - Entra na página que exibe as planilhas
+// 1 - Entra direto na página cadastro
 define('INITIAL_PAGE', 0);
-// EXIBE A ANIMAÇÃO DA CARGA DA PÁGINA
+// Exibe a animação da carga da página
 define('WAIT', 1);
-// 1 Aceita caracteres acentuados na inserção e edição
+// 0 - Remove automaticamente a acentuação ao digitar algo nas caixas de texto
+// 1 - Aceita caracteres acentuados na inserção e edição dos dados
 define('ACENTUACAO', 0);
 // 1 Habilita o módulo financeiro
 define('FINANCAS', 1);
@@ -47,23 +71,21 @@ define('VENDAS', 1);
 define('LGPD', 0);
 // Exibe o rodapé no final da página
 define('FOOTER', 1);
-//
+// Define se estamos no modo desenvolvimento ou produção
 if (strtolower($_SERVER['HTTP_HOST']) == 'localhost') {
     define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-} else {
-    define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
-}
-
-if (strtolower($_SERVER['HTTP_HOST']) == 'localhost') {
     // Modificar para a sua pasta local
     define('URL', 'http://localhost/scw-main/');
 } else {
+    define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
     // Modificar para a pasta do seu servidor
     define('URL', 'https://www.seu_dominio.com.br');
 }
-//
+
+// Define o caminho da página de aviso caso a sessão tenha expirado
 define('SESSION_EXPIRED', URL . 'session_expired/');
-// Define o tempo máximo do script o normal são de 30 segundos
+// Define o tempo máximo de execução de um script, 
+// o normal são de 30 segundos
 ini_set("max_execution_time", 0);
 //      ___              _                     
 //     |   \   __ _   __| |  ___   ___         
@@ -139,13 +161,8 @@ define('ROBOT', 'Este cadastro pode ter sido preenchido automaticamente por um r
 // | _|  \ \ / / _| / -_) (_-< / _ \ / -_) (_-<
 // |___| /_\_\ \__| \___| /__/ \___/ \___| /__/
 //                                              
-if (strtolower($_SERVER['HTTP_HOST']) == 'localhost') {
-    define('MODE', 'DEVELOPMENT');
-} else {
-    define('MODE', 'PRODUCTION');
-}
 //
-switch (MODE) {
+switch (strtolower(ENVIRONMENT)) {
     case 'DEVELOPMENT':
         ini_set('display_errors', 'On');
         error_reporting(E_ALL);
@@ -172,7 +189,7 @@ ini_set('error_log', ROOT . 'log' . DIRECTORY_SEPARATOR . 'errors.log');
  // |  _/ / _` | / _` | | '_| / _` | / _ \
  // |_|   \__,_| \__,_| |_|   \__,_| \___/
                                        
-// Carrega as classes padrão do projeto
+// Carrega as classes que são utilizadas em todas as páginas
 if (!isset($nivel)) {
     $nivel = "";
 }
