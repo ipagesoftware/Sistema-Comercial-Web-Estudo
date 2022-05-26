0773808359a69df1f675b840cd813021e5b6555f
Diógenes Dias <diogenesdias@hotmail.com>
2022-05-26 14:09:10 -0300

    Update EsqueciSenha.js

--- application/views/access/esqueci_senha/js/EsqueciSenha.js
+++ application/views/access/esqueci_senha/js/EsqueciSenha.js
@@ -1,179 +1,131 @@
-  /**
-       * @version    1.0
-       * @package    Acesso
-       * @subpackage Esqueci a senha
-       * @author     Diógenes Dias <diogenesdias@hotmail.com>
-       * @copyright  Copyright (c) 1995-2021 Ipage Software Ltd. (https://www.ipage.com.br)
-       * @license    https://www.ipagesoftware.com.br/license_key/www/examples/license/
-       *
-        Metrics
-          There are 24 functions in this file.
-          Function with the largest signature take 2 arguments, while the median is 0.
-          Largest function has 10 statements in it, while the median is 1.
-          The most complex function has a cyclomatic complexity value of 6 while the median is 1.
-       * 
-      */
-  $(document).ready(function() {
-      esqueciSenha.init();
-  });
-  /*
-   ************************************************************
-   */
-  var esqueciSenha = function() {
-      // Métodos privados da classe
-          var handleEvents = function() {
-          //// Habilita o uso da tecla enter nas caixas de texto
-          //////
-          $('input password').keypress(function(event) {
-              var allInputs = $(':text:visible');
-              if (event.keyCode == 13) {
-                  event.preventDefault();
-                  // A próxima entrada na minha coleção de todas as entradas
-                  var nextInput = allInputs.get(allInputs.index(this) + 1);
-                  //
-                  //// Verifica se é a última caixa de texto
-                  ////////
-                  if ($(this).data('last-input')) {
-                      Login.submitForm();
-                  } else if (nextInput) {
-                      // Passa o foco para próxima entrada se a entrada não for nula
-                      nextInput.focus();
-                  }
-              }
-          });
-      };
-      // 
-      function handleButton() {
-        var time =0;
-          $("#btn_novo").click(function() {
-              window.location.href = 'esqueci_senha/';
-          });
-          $("#btn_captcha").click(function() {
-              esqueciSenha.change_captcha();
-          });
-          $("#btn_enviar").click(function() {
-              $('#form1').submit();
-          });
-          $("#btn_sair").click(function() {
-              window.parent.location.href = 'index.php';
-          });
-          setInterval(function(){
-              time++;
-              // Dois minutos de inatividade
-              if(time >= 120){
-                  time = 0;
-                  esqueciSenha.change_captcha(function(result){
-                      $("#txtkey").val("").focus();
-                  });                
-              }
-          }, 1000);          
-      }
-      /**
-       * Realiza a validação dos dados do form
-       * @return true se a validação foi bem sucedida
-       */
-      function verificaCampos() {
-          var msg = 'Campo inválido ou inexistente, verifique!';
-          var email = $("#txtemail").val();
-          //
-          if (typeof(email) === 'undefined' || email.length === 0 || ipageViews.verificaEmail(email) === false) {
-              ipageViews.notyMessage('Email: ' + msg, 'error', 2000, function(result) {
-                  $("#txtemail").focus();
-              });            
-              return false;
-          }
-          //
-          txtkey = $("#txtkey").val();
-          if (typeof(txtkey) === 'undefined' || txtkey.length === 0) {
-              ipageViews.notyMessage('Código Acesso: ' + msg, 'error', 2000, function(result) {
-                  $("#txtkey").focus();
-              });             
-              return false;
-          }
-          //
-          return true;
-      }
-      /**
-       * Inicialização da classe
-       * @param  parâmetro com os dados do form esqueci a senha
-       * @return void
-       */
-      return {
-          init: function(par) {
-              IpageApp.wait(true);
-              //
-              $('#form1').attr("action", "javascript:esqueciSenha.submitForm();void(0);");
-              handleButton();
-              IpageApp.wait(false);
-          },
-          change_captcha: function(callback) {
-              IpageApp.wait(true);
-              document.getElementById('img-key').src = "application/controles/captcha/pngimg.php?rnd=" + Math.random();
-              //
-              $('#img-key').load(function() {
-                  return callback ? callback(true) : false;
-              });
-          },
-          enviar: function() {
-              var cUrl = new IPAGE_url_encodeClass(),
-                  par = '',
-                  email = $('#txtemail').val(),
-                  captcha = $('#txtkey').val(),
-                  serializeDados = '';
-              par += 'email=' + cUrl.encodeHex($.base64.encode(email));
-              par += '&captcha=' + cUrl.encodeHex($.base64.encode(captcha));
-              //  
-              serializeDados = par;
-              //
-              $.ajax({
-                  type: 'POST',
-                  contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-                  url: "application/views/access/esqueci_senha/ajax/esqueci_senha_validar_ajax.php?rnd=" + Math.random(),
-                  data: serializeDados,
-                  beforeSend: function() {
-                      IpageApp.wait(true);
-                  },
-                success: function(txt, textStatus) {
-                    IpageApp.wait(false);
-                    try {
-                        json = JSON.parse(txt);
-                        msg = json.msg;
-                        id = json.id;
-                    } catch (e) {
-                        msg = txt;
-                        id = 'txtemail'; // Primeira caixa de texto
-                    }
-                    if (msg == "OK") {
-                        ipageViews.notyMessage("Você receberá um email informando a sua nova senha.", 'success', 0, function(result) {
-                            window.location.href = "login/";
-                        });
-                    } else {
-                        ipageViews.notyMessage(msg, 'error', 2000, function(result) {
-                            esqueciSenha.change_captcha(function(result){
-                                if(id!='txtkey'){
-                                    $('#txtkey').val("");
-                                    $('#' + id).trigger('focus').select();
-                                }else{
-                                    $('#txtkey').trigger('focus').select();                                    
-                                }
-                            });
-                        });
-                    }
-                },
-                error: function(xhr, er) {
-                    IpageApp.wait(false);
-                    var ret = 'Error ' + xhr.status + ' - ' + xhr.statustext + '\nTipo de erro: ' + er;
-                    ipageViews.notyMessage(ret, 'error', 2000);
-                    esqueciSenha.change_captcha();
-                }
-              });
-              return false;
-          },
-          submitForm: function() {
-              if (verificaCampos() === false) {
-                  return false;
-              }
-              return esqueciSenha.enviar();
-          }
-      };
-  }();
+<?php
+/**
+ * @version    1.0
+ * @package    Acesso
+ * @subpackage Esqueci a Senha
+ * @author     DiÃ³genes Dias <diogenesdias@hotmail.com>
+ * @copyright  Copyright (c) 1995-2021 Ipage Software Ltd. (https://www.ipage.com.br)
+ * @license    https://www.ipagesoftware.com.br/license_key/www/examples/license/
+ */
+setlocale(LC_TIME, 'portuguese');
+//date_default_timezone_set('America/Recife');
+date_default_timezone_set("Brazil/East");
+//
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+class EsqueciSenhaValidar
+{
+    public $email;
+    public $newpwd;
+
+    /**
+     * [__construct description]
+     */
+    public function __construct()
+    {
+        $this->newpwd = substr(str_shuffle("AaBbCcDdEeFfGgHhiJjKkLMmNnPpQqRrSsTtUuVvYyXxWwZz23456789"), 0, (8));
+    }
+
+    /**
+     * CaptaÃ§Ã£o dos dados vindos do form
+     * @return string
+     */
+    public function getValues()
+    {     
+        $email   = $_POST['email'];
+        $captcha = $_POST['captcha'];
+        // Decodifica dados
+        $email   = $this->decodeGET($email);
+        //
+        // Inicia as validaÃ§Ãµes
+        // Verifica se o email Ã© vÃ¡lido
+        // Remove os caracteres ilegais do email
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        //
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
+            $json = array('id' => 'txtemail',
+                'msg'=> utf8_encode('O email possui caracteres invÃ¡lidos ou foi digitado incorretamente, verifique!'),
+            );
+            return (json_encode($json));
+        }
+        //
+        $this->email = $email;
+        //
+        if(CAPTCHA){
+            // Verifica se o captcha Ã© vÃ¡lido
+            $captcha = $_POST['captcha'];
+            $captcha = $this->decodeGET($captcha);
+            $ret = $this->getStatusCaptcha($captcha);
+            //
+            if (is_null($ret)) {
+                $json = array('id' => 'txtkey',
+                    'msg'=> utf8_encode('CÃ³digo acesso invÃ¡lido, verifique!'),
+                );
+                return (json_encode($json));
+            } 
+        } 
+        return 'OK';
+    }
+
+    /**
+     * [getStatusCaptcha description]
+     * @param  [type] $_captcha [description]
+     * @return [type]           [description]
+     */
+    private function getStatusCaptcha($_captcha)
+    {
+        if (!$_captcha)return null;
+        //
+        $ret = strcasecmp($_captcha, $this->getCaptcha());
+        //
+        if ($ret!=0) {
+            return null;
+        } else {
+            return 1;
+        }
+    }
+
+    /**
+     * Retorna o captcha gerado pelo sistema
+     *
+     * @return string
+     */
+    private function getCaptcha()
+    {
+        if (!isset($_SESSION["ckey"])) {
+            $_SESSION["ckey"] = 'undefined';
+        }
+        //
+        return $_SESSION["ckey"];
+    }
+
+    /**
+     * Decodifica uma string em hexadecimal
+     * @param  string hexadecimal
+     * @return string decodificada
+     */
+    private function decodeHex($hex)
+    {
+        $string = '';
+        //
+        for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
+            $string .= chr(hexdec($hex[$i] . $hex[$i + 1]));
+        }
+        //
+        return $string;
+    }
+
+    /**
+     * Decodifica uma url get codificada
+     * @param  string url codificada
+     * @return String decodificada
+     */
+    private function decodeGET($value)
+    {
+        $ret   = $this->decodeHex($value);
+        $value = fulltrim(base64_decode($ret));
+        return $value;
+    }
+}
